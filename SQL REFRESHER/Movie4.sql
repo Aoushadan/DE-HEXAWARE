f13fb8a7fad015c406fbd0b1f46be81499d6eb6c
Aoushadan <132703781+Aoushadan@users.noreply.github.com>
2025-07-18 09:16:04 +0530

    Add files via upload

---
+++ SQL REFRESHER/Movie4.sql
@@ -0,0 +1,57 @@
+
+
+/*DELETE FROM ShowSeat WHERE show_id = 1 AND seat_id = 1;
+
+
+DELETE FROM ShowSeat WHERE show_id = 1;
+
+
+DELETE FROM Show WHERE show_id = 1;
+
+
+
+DELETE FROM Seat WHERE seat_number = 'A1';
+
+
+DELETE FROM ShowSeat WHERE show_id IN (SELECT show_id FROM Show WHERE screen_id = 1);
+
+
+DELETE FROM Seat WHERE screen_id = 1;
+
+DELETE FROM Screen WHERE screen_id = 1;
+
+DELETE FROM Seat WHERE screen_id = 1;
+
+DELETE FROM MovieCast WHERE person_name = 'Chadwick Boseman';
+
+
+DELETE FROM MovieCast WHERE movie_id = 1;
+
+
+DELETE FROM Movie WHERE movie_id = 1;
+
+
+
+DELETE FROM Review WHERE reviewer_name = 'John Doe';
+
+
+DELETE FROM Booking WHERE booking_id = 1;
+
+
+DELETE FROM Membership WHERE user_id = 1;
+
+
+DELETE FROM [User] WHERE user_id = 1;
+*/
+
+
+ALTER TABLE Screen
+ADD Popcorn varchar(55)
+
+DELETE FROM Seat WHERE screen_id = 5;
+
+DELETE FROM Screen WHERE screen_id = 6;
+
+DELETE FROM Seat WHERE screen_id = 7;
+
+DELETE FROM MovieCast WHERE person_name = 'Aoushadan';
